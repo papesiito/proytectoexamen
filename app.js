c90ec079da89f10b8e3ade0ea1f83ed64ce13070
papesiito <123887877+papesiito@users.noreply.github.com>
2025-05-12 18:44:15 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res) => {
+  res.write('Hola Mundo desde el servidor!');
+  res.end();
+})
+.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
